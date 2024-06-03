75a8bf79645bffc3afede665cf067c169fb48726
Guilherme do Carmo <docarmog32@gmail.com>
2024-06-03 17:20:25 -0300

    Loop
    Envolve o código principal para que o usuário possa realizar múltiplas operações sem reiniciar o programa.

--- src/Main.kt
+++ src/Main.kt
@@ -2,40 +2,44 @@ import kotlin.math.ln1p
 import kotlin.system.exitProcess
 
 fun main() {
+    while(true) {
+        println("------------------------- Calculadora -------------------------")
+        var n1 = 1
+        var n2 = 1
+        n1 = getInput("Insira um número: ")
+        n2 = getInput("Insira outro número: ")
 
-    println("------------------------- Calculadora -------------------------")
-    var n1 = 1
-    var n2 = 1
-    n1 = getInput("Insira um número: ")
-    n2 = getInput("Insira outro número: ")
-
-    println("""Escolha uma das seguintes operações
+        println("""Escolha uma das seguintes operações
         |+ adição
         |- subtração
         |* multiplicação
         |/ divisão
     """.trimMargin())
-    print("> ")
-    var op = readln()
+        print("> ")
+        var op = readln()
 
-    println()
+        println()
 
-    try {
-        when (op) {
-            "+" -> println("$n1 + $n2 = ${n1 + n2}")
-            "-" -> println("$n1 - $n2 = ${n1 - n2}")
-            "*" -> println("$n1 * $n2 = ${n1 * n2}")
-            "/" -> {
-                if (n2 == 0) {
-                    println("Erro: Divisão por zero não é permitida.")
-                } else {
-                    println("$n1 / $n2 = ${n1 / n2}")
+        try {
+            when (op) {
+                "+" -> println("$n1 + $n2 = ${n1 + n2}")
+                "-" -> println("$n1 - $n2 = ${n1 - n2}")
+                "*" -> println("$n1 * $n2 = ${n1 * n2}")
+                "/" -> {
+                    if (n2 == 0) {
+                        println("Erro: Divisão por zero não é permitida.")
+                    } else {
+                        println("$n1 / $n2 = ${n1 / n2}")
+                    }
                 }
+                else -> throw Exception("Operador INVÁLIDO!")
             }
-            else -> throw Exception("Operador INVÁLIDO!")
+        } catch (e: Exception) {
+            println("Erro: ${e.message}")
         }
-    } catch (e: Exception) {
-        println("Erro: ${e.message}")
+
+        println("Deseja realizar outra operação? (s/n)")
+        if (readln().lowercase() != "s") break
     }
 
 }
